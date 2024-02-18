18ee117ec317687bd85daf945bd8c60db2638d89
Andrey K <andrey0kozyrev0@gmail.com>
2024-02-19 02:12:19 +0300

    tests. small lint fixes

--- app/tests/test_apps/users/test_model_users.py
+++ app/tests/test_apps/users/test_model_users.py
@@ -7,5 +7,5 @@ pytestmark = [pytest.mark.django_db]
 
 def test_create(get_user_data, generic_model_factory, assert_model_and_expected_data):
     user_data = get_user_data()
-    user = generic_model_factory(model=User, **user_data)
+    user = generic_model_factory(User, **user_data)
     assert_model_and_expected_data(user, user_data)
--- app/tests/test_apps/users/test_user_service.py
+++ app/tests/test_apps/users/test_user_service.py
@@ -9,6 +9,6 @@ def test_get_by_email(
     user_service, get_user_data, generic_model_factory, assert_model_and_expected_data
 ):
     user_data = get_user_data()
-    generic_model_factory(model=User, **user_data)
+    generic_model_factory(User, **user_data)
     assert_user = user_service.get_by_email(user_data['email'])
     assert_model_and_expected_data(assert_user, user_data)
